efe2a7195d15743d47673d04167010b9dd84608a
Paul Osborne <osbpau@gmail.com>
2014-01-17 00:59:36 -0600

    sync: first working version of file sychronization
    There is definitely quite a bit of work to do, but right now this
    is a working file sychronization client.

--- putiosync.py
+++ putiosync.py
@@ -3,7 +3,11 @@
 # Program for automatically downloading and removing files that are
 # successfully downloaded from put.io.
 #
+import Queue
+import collections
 import json
+import progressbar
+import re
 import webbrowser
 import time
 import sys
@@ -65,6 +69,20 @@ def parse_arguments():
     return args
 
 
+class DownloadQueue(object):
+    """Store queued downloads"""
+
+    def __init__(self):
+        self._queue = collections.deque()
+
+    def add(self, putio_file):
+        """Add the specified file to the download queue"""
+        self._queue.append(putio_file)
+
+    def __iter__(self):
+        return iter(list(self._queue))
+
+
 class PutioSynchronizer(object):
     """Object encapsulating core synchronization logic and state"""
 
@@ -72,16 +90,64 @@ class PutioSynchronizer(object):
         self._token = token
         self._download_directory = download_directory
         self._putio_client = putio.Client(token)
+        self._download_queue = DownloadQueue()
+
+    def _is_directory(self, putio_file):
+        return (putio_file.content_type == 'application/x-directory')
+
+    def _do_download(self, putio_file, dest, delete_after_download=False):
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+        response = putio_file.client.request(
+            '/files/%s/download' % putio_file.id,
+            raw=True, stream=True)
+        filename = re.match(
+            'attachment; filename=(.*)',
+            response.headers['content-disposition']).groups()[0]
+        filename = filename.strip('"')
+
+        total = putio_file.size
+        downloaded = 0
+        widgets = [
+            progressbar.Percentage(), ' ',
+            progressbar.Bar(), ' ',
+            progressbar.ETA(), ' ',
+            progressbar.FileTransferSpeed()]
+        pbar = progressbar.ProgressBar(widgets=widgets, maxval=total).start()
+        with open(os.path.join(dest, filename), 'wb') as f:
+            for chunk in response.iter_content(chunk_size=256):
+                if chunk:  # filter out keep-alive new chunks
+                    downloaded += len(chunk)
+                    pbar.update(downloaded)
+                    f.write(chunk)
+                    f.flush()
+
+        if delete_after_download:
+            putio_file.delete()
+
+    def _download_and_delete(self, putio_file, relpath="", level=0):
+        print "Downloading {}{}".format("  " * level, putio_file)
+        # add this file (or files in this directory) to the queue
+        if not self._is_directory(putio_file):
+            target_dir = os.path.join(self._download_directory, relpath)
+            self._do_download(putio_file, target_dir, delete_after_download=True)
+        else:
+            for child in putio_file.dir():
+                self._download_and_delete(
+                    child, os.path.join(relpath, putio_file.name), level + 1)
+            putio_file.delete()  # children already downloaded
 
     def _perform_single_check(self):
         # Perform a single check for updated files to download
-        print self._putio_client.File.list()
+        for putio_file in self._putio_client.File.list():
+            self._download_and_delete(putio_file)
 
     def run_forever(self):
         """Run the synchronizer until killed"""
         while True:
             self._perform_single_check()
             time.sleep(CHECK_PERIOD_SECONDS)
+
 
 def main():
     args = parse_arguments()
