61a5c6f3792103ea3c97469920f294f3b16be16f
Josh Kerr <joshkerr@gmail.com>
2019-02-17 09:55:35 -0600

    Update core.py

--- putiosync/core.py
+++ putiosync/core.py
@@ -220,7 +220,7 @@ class PutioSynchronizer(object):
             else:
                 logger.error("Directory with children")
                 for child in children:
-                    self._queue_download(child, os.path.join(relpath, putio_file.name), level + 1)
+                    self._queue_download(child, os.path.join(relpath, putio_file.name.encode('utf-8','ignore')), level + 1)
 
     def _perform_single_check(self):
         try:
